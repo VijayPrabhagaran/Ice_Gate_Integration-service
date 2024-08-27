9fb320557a2cc2dfb71094ed38b405581bc40c87
RajubaiV <rajubaiv08@gmail.com>
2024-08-27 09:56:52 +0530

    Created service for ArrivalDepature

---
+++ IGI.Icegate_Integration.Core/Domain.Entities/ArrivalDepartureRequestDetails.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IGI.Icegate_Integration.Core.Domain.Entities
+{
+    public class ArrivalDepartureRequestDetails
+    {
+        [Key]
+        public int ArrivalDepartureDetailsId { get; set; }
+        public string? AckId { get; set; }
+        public string? RequestorId { get; set; }
+        public string? RecordType { get; set; }
+        public string? PortCode { get; set; }
+        public string? VoyageCallNumber { get; set; }
+        public string? TerminalOperatorCode { get; set; }
+        public string? ReportingEvent { get; set; }
+        public string? TypeOfTransportMeans { get; set; }
+        public string? IdentityOfTransportMeans { get; set; }
+        public string? BerthNumber { get; set; }
+        public string? ManifestNumber { get; set; }
+        public DateTimeOffset? ManifestDate { get; set; }
+        public string? ARTerminalOperator { get; set; }
+        public DateTimeOffset? ArrivalDepartureTime { get; set; }
+        public string? FinalAmendsArrivalDepartureTime { get; set; }
+        public string? StateId { get; set; }
+    }
+    public class ArrivalDepartureRequestDetailsJson
+    {
+        public ArrivalDepartureRequestDetails? Text { get; set; }
+    }
+}
---
+++ IGI.Icegate_Integration.Core/Domain.Entities/ArrivalDepartureResponseDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IGI.Icegate_Integration.Core.Domain.Entities
+{
+    public class ArrivalDepartureResponseDetails
+    {
+        [Key]
+        public int ArrivalDepartureResponseId { get; set; }
+        public string? Status { get; set; }
+        public string? ErrorCode { get; set; }
+        public string? CommonRefNumber { get; set; }
+        public string? Message { get; set; }
+        public string? RequestorId { get; set; }
+        public string? AckId { get; set; }
+    }
+}
